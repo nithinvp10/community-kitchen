276f472bcf314efce47cf649fa8b8289b1da17d1
vivinkv6 <vivinkv0@gmail.com>
2024-01-19 17:12:04 +0530

    create add product,delete product, logout routes

---
+++ models/admin/product.js
@@ -0,0 +1,29 @@
+const {DataTypes}=require('sequelize');
+const sequelizeConfig=require('../../config/sequelize.config');
+
+const productModel=sequelizeConfig.define('product',{
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name:{
+          type: DataTypes.STRING,
+          allowNull: false, 
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      image:{
+        type: DataTypes.STRING,
+        allowNull: false, 
+      }
+});
+
+module.exports=productModel;
